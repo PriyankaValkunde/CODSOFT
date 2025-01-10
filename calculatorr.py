6d397a153c0c5a3bca8d41beea690fa31106e6cc
PriyankaValkunde <156121451+PriyankaValkunde@users.noreply.github.com>
2025-01-10 14:09:01 +0530

    calculatorr.py

--- calculatorr.py
+++ calculatorr.py
@@ -1,41 +1,115 @@
-def add(num1, num2):
-	return num1 + num2
+ from tkinter import *
+expression = "" 
 
-def subtract(num1, num2):
-	return num1 - num2
+def press(num): 
+	global expression 
 
-def multiply(num1, num2):
-	return num1 * num2
+	expression = expression + str(num) 
 
-def divide(num1, num2):
-	return num1 / num2
+	equation.set(expression) 
 
-print("Please select operation -\n" \
-		"1. Add\n" \
-		"2. Subtract\n" \
-		"3. Multiply\n" \
-		"4. Divide\n")
 
-select = int(input("Select operations form 1, 2, 3, 4 :"))
 
-number_1 = int(input("Enter first number: "))
-number_2 = int(input("Enter second number: "))
+def equalpress(): 
+	try: 
 
-if select == 1:
-	print(number_1, "+", number_2, "=",
-					add(number_1, number_2))
+		global expression 
+		total = str(eval(expression)) 
+		equation.set(total) 
+		expression = "" 
 
-elif select == 2:
-	print(number_1, "-", number_2, "=",
-					subtract(number_1, number_2))
+	except: 
 
-elif select == 3:
-	print(number_1, "*", number_2, "=",
-					multiply(number_1, number_2))
+		equation.set(" error ") 
+		expression = "" 
 
-elif select == 4:
-	print(number_1, "/", number_2, "=",
-					divide(number_1, number_2))
-else:
-	print("Invalid input")
+def clear(): 
+	global expression 
+	expression = "" 
+	equation.set("") 
 
+if __name__ == "__main__": 
+	
+	gui = Tk() 
+
+	gui.configure(background="light green") 
+
+	gui.title("Simple Calculator") 
+
+	gui.geometry("270x150") 
+
+	equation = StringVar() 
+
+	expression_field = Entry(gui, textvariable=equation) 
+
+	expression_field.grid(columnspan=4, ipadx=70)
+
+	button1 = Button(gui, text=' 1 ', fg='black', bg='red', 
+					command=lambda: press(1), height=1, width=7) 
+	button1.grid(row=2, column=0) 
+
+	button2 = Button(gui, text=' 2 ', fg='black', bg='red', 
+					command=lambda: press(2), height=1, width=7) 
+	button2.grid(row=2, column=1) 
+
+	button3 = Button(gui, text=' 3 ', fg='black', bg='red', 
+					command=lambda: press(3), height=1, width=7) 
+	button3.grid(row=2, column=2) 
+
+	button4 = Button(gui, text=' 4 ', fg='black', bg='red', 
+					command=lambda: press(4), height=1, width=7) 
+	button4.grid(row=3, column=0) 
+
+	button5 = Button(gui, text=' 5 ', fg='black', bg='red', 
+					command=lambda: press(5), height=1, width=7) 
+	button5.grid(row=3, column=1) 
+
+	button6 = Button(gui, text=' 6 ', fg='black', bg='red', 
+					command=lambda: press(6), height=1, width=7) 
+	button6.grid(row=3, column=2) 
+
+	button7 = Button(gui, text=' 7 ', fg='black', bg='red', 
+					command=lambda: press(7), height=1, width=7) 
+	button7.grid(row=4, column=0) 
+
+	button8 = Button(gui, text=' 8 ', fg='black', bg='red', 
+					command=lambda: press(8), height=1, width=7) 
+	button8.grid(row=4, column=1) 
+
+	button9 = Button(gui, text=' 9 ', fg='black', bg='red', 
+					command=lambda: press(9), height=1, width=7) 
+	button9.grid(row=4, column=2) 
+
+	button0 = Button(gui, text=' 0 ', fg='black', bg='red', 
+					command=lambda: press(0), height=1, width=7) 
+	button0.grid(row=5, column=0) 
+
+	plus = Button(gui, text=' + ', fg='black', bg='red', 
+				command=lambda: press("+"), height=1, width=7) 
+	plus.grid(row=2, column=3) 
+
+	minus = Button(gui, text=' - ', fg='black', bg='red', 
+				command=lambda: press("-"), height=1, width=7) 
+	minus.grid(row=3, column=3) 
+
+	multiply = Button(gui, text=' * ', fg='black', bg='red', 
+					command=lambda: press("*"), height=1, width=7) 
+	multiply.grid(row=4, column=3) 
+
+	divide = Button(gui, text=' / ', fg='black', bg='red', 
+					command=lambda: press("/"), height=1, width=7) 
+	divide.grid(row=5, column=3) 
+
+	equal = Button(gui, text=' = ', fg='black', bg='red', 
+				command=equalpress, height=1, width=7) 
+	equal.grid(row=5, column=2) 
+
+	clear = Button(gui, text='Clear', fg='black', bg='red', 
+				command=clear, height=1, width=7) 
+	clear.grid(row=5, column='1') 
+
+	Decimal= Button(gui, text='.', fg='black', bg='red', 
+					command=lambda: press('.'), height=1, width=7) 
+	Decimal.grid(row=6, column=0) 
+	# start the GUI 
+	gui.mainloop() 
